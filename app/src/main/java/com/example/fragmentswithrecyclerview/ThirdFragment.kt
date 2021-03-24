3aded1dc90efb894f78bafc9f90d603ba82c853a
mariklolik <marshelo44@gmail.com>
2021-03-24 17:20:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragmentswithrecyclerview/ThirdFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fragmentswithrecyclerview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragmentswithrecyclerview.dummy.DummyContent
+
+/**
+ * A fragment representing a list of Items.
+ */
+class ThirdFragment : Fragment() {
+
+}
